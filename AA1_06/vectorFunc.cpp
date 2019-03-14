de2c5f50819eb498628e0e79771e2e900a108a2f
victor <victor.99@hotmail.es>
2019-03-14 18:01:45 +0100

    AA1_06

--- AA1_06/vectorFunc.cpp
+++ AA1_06/vectorFunc.cpp
@@ -17,15 +17,14 @@ std::vector<int> addVectors(std::vector<int> v1, std::vector<int> v2)
 std::vector<std::string> concatVectors(std::vector<std::string> v1, std::vector<std::string> v2)
 {
 	int counter = 0;
-	std::vector<std::string> v3;
+	std::vector<std::string> v3(v1);
 
-	for (int i = 0; i < v1.size(); i++)
-		v3.push_back(v1[i]);
 	
-	for (int j = v1.size(); j < v1.size() + v2.size(); j++)
+	
+	for (int i = 0; i < v2.size(); i++)
 	{
-		v3.push_back(v2[counter]);
-		counter++;
+		v3.push_back(v2[i]);
+		
 	}
 
 
